098eb9ce23a4e2a936ec413eebdef1caaf2e8ccd
Christian Lombardi <96151080+Znorlux@users.noreply.github.com>
2023-03-01 09:19:06 -0500

    Delete Examen Codigo Limpio directory

--- Examen Codigo Limpio/CorreoInvalidoError.java
+++
@@ -1,9 +0,0 @@
-public class CorreoInvalidoError extends Exception {
-    public CorreoInvalidoError() {
-        super();
-    }
-    public CorreoInvalidoError(String message) {
-        super(message);
-        System.out.println(message);
-    }
-}
